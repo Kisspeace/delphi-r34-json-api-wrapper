e18fa53020224aa76b1d8f719c5544db37256865
kisspeace <kisspeace@github.com>
2022-06-20 02:39:08 -0400

    init

---
+++ source/NetHttp.R34JsonApi.pas
@@ -0,0 +1,67 @@
+﻿//♡2022 by Kisspeace. https://github.com/kisspeace
+unit NetHttp.R34JsonApi;
+
+interface
+uses
+  XSuperObject, Sysutils, Net.HttpClient,
+  Net.HttpClientComponent, R34JsonApi.Types;
+
+const
+  R34_API_URL = 'https://r34-json-api.herokuapp.com';
+
+type
+
+  TR34Client = class(TObject)
+    public
+      WebClient: TNetHttpClient;
+      function GetPosts(ATags: string; APid: integer = 1; ALimit: byte = 100): TR34Items;
+      function GetComments(APostId: string): TR34Comments;
+      constructor Create;
+      destructor Destroy; override;
+  end;
+
+implementation
+
+{ TR34Client }
+
+constructor TR34Client.Create;
+begin
+  WebClient := TNetHttpClient.Create(nil);
+end;
+
+destructor TR34Client.Destroy;
+begin
+  WebClient.Free;
+  inherited;
+end;
+
+function TR34Client.GetComments(APostId: string): TR34Comments;
+var
+  X: ISuperArray;
+  Response: IHTTPResponse;
+begin
+  Result := nil;
+  Response := WebClient.Get(R34_API_URL + '/comments?post_id=' + APostId);
+  X := SA(Response.ContentAsString);
+  Result := TJson.Parse<TR34Comments>(X);
+end;
+
+function TR34Client.GetPosts(ATags: string; APid: integer;
+  ALimit: byte): TR34Items;
+var
+  X: ISuperArray;
+  Response: IHTTPResponse;
+begin
+  Result := nil;
+  Response := WebClient.Get(R34_API_URL + '/posts?tags=' + ATags +
+   '&pid=' + APid.ToString + '&limit=' + Alimit.ToString);
+
+  try
+    X := SA(Response.ContentAsString);
+    Result := TJson.Parse<TR34Items>(X);
+  except
+    Result := nil;
+  end;
+end;
+
+end.
---
+++ source/R34JsonApi.Types.pas
@@ -0,0 +1,105 @@
+﻿//♡2022 by Kisspeace. https://github.com/kisspeace
+unit R34JsonApi.Types;
+
+interface
+uses
+  Sysutils;
+
+type
+
+  TR34Item = record
+    height: shortstring;
+    score: shortstring;
+    file_url: string;
+    parent_id: shortstring;
+    sample_url: string;
+    sample_width: shortstring;
+    sample_height: shortstring;
+    preview_url: string;
+    rating: shortstring;
+    id: shortstring;
+    width: shortstring;
+    tags: TArray<string>;
+    change: string;
+    md5: shortstring;
+    creator_id: shortstring;
+    has_children: string;
+    created_at: string;
+    status: string;
+    source: string;
+    has_notes: string;
+    has_comments: string;
+    preview_width: shortstring;
+    preview_height: shortstring;
+    comments_url: string;
+    [ALIAS('type')]
+    typ: string;
+    creator_url: string;
+    function GetHeight: integer;
+    function GetWidth: integer;
+    function GetScore: integer;
+    function HasChildren: boolean;
+    function HasComments: boolean;
+    function HasNotes: boolean;
+    function TagsStr: string;
+  end;
+
+  TR34Comment = record
+    created_at: string;
+    post_id: shortstring;
+    body: string;
+    creator: string;
+    id: shortstring;
+    creator_id: shortstring;
+    post_url: string;
+  end;
+
+  TR34Items = TArray<TR34Item>;
+  TR34Comments = TArray<TR34Comment>;
+
+
+implementation
+
+{ TR34Item }
+
+function TR34Item.GetHeight: integer;
+begin
+  TryStrToInt(height, result);
+end;
+
+function TR34Item.GetScore: integer;
+begin
+  TryStrToInt(score, result);
+end;
+
+function TR34Item.GetWidth: integer;
+begin
+  TryStrToInt(width, result);
+end;
+
+function TR34Item.HasChildren: boolean;
+begin
+  Result := (has_children = 'true');
+end;
+
+function TR34Item.HasComments: boolean;
+begin
+  Result := (has_comments = 'true');
+end;
+
+function TR34Item.HasNotes: boolean;
+begin
+  Result := (has_notes = 'true');
+end;
+
+function TR34Item.TagsStr: string;
+var
+  I: integer;
+begin
+  Result := '';
+  for I := Low(tags) to High(tags) do begin
+    Result := Result + ' ' + Tags[i];
+  end;
+end;
+
+end.
